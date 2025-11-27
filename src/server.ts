09705e9f3e99d1b2d9ee07eef422f3fb5acd33ba
ricashtrans <ricashtrans@gmail.com>
2025-11-27 16:34:33 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
+import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 import app from "./app";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log(" Connexion à la base PostgreSQL réussie");
-    app.listen(PORT, () => console.log(` Serveur démarré sur le port ${PORT}`));
-  })
-  .catch((err) => console.error("Erreur de connexion :", err));
+async function startServer() {
+  console.log("⏳ Initialisation du service de notifications...");
+
+  try {
+    await AppDataSource.initialize();
+    console.log("🟢 Connexion PostgreSQL réussie.");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Notification-Service démarré sur le port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Erreur lors de la connexion PostgreSQL :", error);
+    console.log("🔁 Nouvelle tentative dans 5 secondes...");
+    setTimeout(startServer, 5000);
+  }
+}
+
+startServer();
